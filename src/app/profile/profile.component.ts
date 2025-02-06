34fae98b3d874ffa660d8fbe435006b0f8862f68
pmylsamy <prabhakaran.mylsamy@solenix.ch>
2025-02-06 23:57:05 +0100

    Profile component changes with form control options

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,14 +1,15 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, AsyncPipe],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
 export class ProfileComponent {
 
-  name = new FormControl('karan', {updateOn: 'blur'});
+  name = new FormControl('karan', {updateOn: 'blur', nonNullable: true});
 
 }
