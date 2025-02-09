a97eb55fc71a797ad5ce3769cac362d91fa1e6c9
pmylsamy <prabhakaran.mylsamy@solenix.ch>
2025-02-09 21:50:39 +0100

    template reference variable, signal view child to access the host elements and updated styles using renderer.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,5 +1,5 @@
 import { AsyncPipe } from '@angular/common';
-import { AfterViewInit, Component, ElementRef, inject, Renderer2 } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, Renderer2, viewChild } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
@@ -9,15 +9,21 @@ import { FormControl, ReactiveFormsModule } from '@angular/forms';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements AfterViewInit {
-  private readonly elementRef = inject(ElementRef);
   private readonly renderer = inject(Renderer2);
+
+  resetButton = viewChild<ElementRef>('resetButton');
+  valid = viewChild<ElementRef>('valid');
 
   name = new FormControl('karan', {updateOn: 'blur', nonNullable: true});
 
   ngAfterViewInit() {
-    const buttonElement = this.elementRef.nativeElement.querySelector('button');
-    if (buttonElement) {
-      this.renderer.setStyle(buttonElement, 'color', 'blue');
+    const button = this.resetButton();
+    if (button) {
+      this.renderer.setStyle(button.nativeElement, 'color', 'blue');
+    }
+    const valid = this.valid();
+    if (valid) {
+      this.renderer.setStyle(valid.nativeElement, 'color', 'orange');
     }
   }
 
