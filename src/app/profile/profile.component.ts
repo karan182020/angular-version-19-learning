57a956e17316c324279033775e0b0d17efaf967f
pmylsamy <prabhakaran.mylsamy@solenix.ch>
2025-02-09 21:42:00 +0100

    ElementRef, Renderer2 injection to set the style on the button element.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,15 +1,24 @@
 import { AsyncPipe } from '@angular/common';
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, Renderer2 } from '@angular/core';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   imports: [ReactiveFormsModule, AsyncPipe],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrls: ['./profile.component.scss']
 })
-export class ProfileComponent {
+export class ProfileComponent implements AfterViewInit {
+  private readonly elementRef = inject(ElementRef);
+  private readonly renderer = inject(Renderer2);
 
   name = new FormControl('karan', {updateOn: 'blur', nonNullable: true});
+
+  ngAfterViewInit() {
+    const buttonElement = this.elementRef.nativeElement.querySelector('button');
+    if (buttonElement) {
+      this.renderer.setStyle(buttonElement, 'color', 'blue');
+    }
+  }
 
 }
